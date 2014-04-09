680a15a54395a3554225789661f077857a9ca313
Roman Gaufman <roman@xanview.com>
2014-04-09 08:16:24 +0100

    increase buffer to 2000000 like before

--- liveMedia/MultiFramedRTPSource.cpp
+++ liveMedia/MultiFramedRTPSource.cpp
@@ -73,7 +73,7 @@ MultiFramedRTPSource
   fReorderingBuffer = new ReorderingPacketBuffer(packetFactory);
 
   // Try to use a big receive buffer for RTP:
-  increaseReceiveBufferTo(env, RTPgs->socketNum(), 50*1024*10);
+  increaseReceiveBufferTo(env, RTPgs->socketNum(), 2000000);
 }
 
 void MultiFramedRTPSource::reset() {
